b9c20a096d3a6119a805f8950f28a9c10c399729
Philip <philip.cai.mao@hotmail.ca>
2021-11-03 04:30:30 -0400

    camera now slows down somewhat

--- Assets/Scripts/PlayerCameraControl.cs
+++ Assets/Scripts/PlayerCameraControl.cs
@@ -6,7 +6,8 @@ public class PlayerCameraControl : MonoBehaviour
 {
     // Config
     public Vector3 targetLocation;
-    public float spacing = 0.05f;
+    public float offset = 2.5f;
+    public float spacing = 7.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +21,19 @@ public class PlayerCameraControl : MonoBehaviour
         Vector3 backwards = transform.forward * -1;
         Ray ray = new Ray(transform.parent.position + new Vector3(targetLocation.x, targetLocation.y, 0), backwards);
         RaycastHit hitInfo = new RaycastHit();
-        if (Physics.Raycast(ray, out hitInfo, Math.Abs(targetLocation.z) + spacing))
+        if (Physics.Raycast(ray, out hitInfo, offset + spacing))
         {
-            float adjustedZ = -(hitInfo.distance - spacing);
-            transform.localPosition = new Vector3(targetLocation.x, targetLocation.y, adjustedZ);
+            float cameraDistance;
+            if (hitInfo.distance > offset)
+            {
+                Debug.Log((1 - (hitInfo.distance - offset) / spacing));
+                cameraDistance = -targetLocation.z - (1-(hitInfo.distance - offset) / spacing) * -targetLocation.z;
+            }
+            else
+            {
+                cameraDistance = hitInfo.distance - offset;
+            }
+            transform.localPosition = new Vector3(targetLocation.x, targetLocation.y, -cameraDistance);
         }
         else
         {
