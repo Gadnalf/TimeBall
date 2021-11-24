c8ae5d0d47aed69516e77fd44372266215435b46
Mohammad Hasan <mohd.hasan@mail.utoronto.ca>
2021-11-24 06:56:18 -0500

    updated chargeleveldisplayer, gameconfigs

--- Assets/Scripts/GameConfigurations.cs
+++ Assets/Scripts/GameConfigurations.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 public static class GameConfigurations
 {
     public static float baseMovementSpeed = 25f;
@@ -6,23 +7,24 @@ public static class GameConfigurations
     public static float rotationSpeed = 400f;
     public static float passAngle = 90f;
 
-    public static float roundDuration = 10f;
+    public static float roundDuration = 25f;
     public static float roundLengthIncrease = 0f;
     public static float maxRoundLength = 25f;
     public static int numberOfRounds = 5;
 
     public static float ballDistance = 2f;
     public static float ballHeight = 2f;
-    public static float ballChargeTime = 0.8f;
-    public static int goalShieldBreakableCharge = 3;
+    public static float ballChargeTime = 1.5f;
+    public static int goalShieldBreakableCharge = 1;
+    public static int maxCloneAutoCharge = 3;
     public static int maxBallCharge = 5;
     public static int cloneBaseCharge = 1;
-    public static float cloneBaseChargeCDInSeconds = 5;
+    public static float cloneBaseChargeCDInSeconds = 10;
 
     public static float horizontalThrowingForce = 2000f;
-    public static float verticalThrowingForce = 200f;
+    public static float verticalThrowingForce = 250f;
 
-    public static float speedBoostFactor = 1.1f;
+    public static float speedBoostFactor = 1.25f;
 
     public static float dashSpeed = 50f;
     public static int dashingFrame = 10;
@@ -35,5 +37,30 @@ public static class GameConfigurations
     public static float stunningSpeed = 1f;
     public static int stunningFrame = 10;
 
+    public static float guardExpandRate = 0.2f;
+    public static float guardMaxSize = 5f;
+    public static float haltRate = 0.8f;
+
     public static int cloneMaxPauseFrames = 100;
+
+    public static Color FromChargeToColor(int chargeLevel)
+    {
+        switch (chargeLevel)
+        {
+            case 0:
+                return Color.white;
+            case 1:
+                return Color.red;
+            case 2:
+                return new Color(1f, 0.75f, 0f); // orange
+            case 3:
+                return Color.yellow;
+            case 4:
+                return new Color(0f, 1f, 0.6f); // light green
+            case 5:
+                return Color.green;
+            default:
+                return Color.black;
+        }
+    }
 }
